527a49028f1565e49e173148da383e9f6930f1e2
Georgekutty Joyes <georgekutty@hifx.co.in>
2018-03-01 18:06:20 +0530

    job modified [now all tasks will start with out asking user confirmation]

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 
 	"github.com/fatih/color"
 )
 
-//InputValues provides struct for unmarshalling request info
-type InputValues struct {
+const inputFile = "conf/conf.json"
+
+//inputValues provides struct for unmarshalling request info
+type inputValues struct {
 	Jobs []struct {
 		Name         string   `json:"name"`
 		Commands     []string `json:"commands"`
@@ -25,73 +25,55 @@ type InputValues struct {
 
 func main() {
 
-	// ----color output pointers
-	msg := color.New(color.FgHiCyan).Add(color.Bold)
-	errMsg := color.New(color.FgRed).Add(color.Bold)
+	// ----sys out colors
+	errMsg := color.New(color.FgRed)
 
 	// ----read json input file
-	reqData, err := os.Open("conf/conf.json")
+	reqData, err := os.Open(inputFile)
 	if err != nil {
-		errMsg.Println(err.Error())
-		os.Exit(1)
+		errMsg.Println("#Error: ", err.Error())
+		os.Exit(0)
 	}
 
 	// ----decoder
 	reqParser := json.NewDecoder(reqData)
-	reqJSON := InputValues{}
+	reqJSON := inputValues{}
 	err = reqParser.Decode(&reqJSON)
 	if err != nil {
-		errMsg.Println(err.Error())
-		os.Exit(1)
+		errMsg.Println("#Error: ", err.Error())
+		os.Exit(0)
 	}
+
+	// ----running shell
+	shell := "guake"
 
 	// ----processing each jobs
 	for _, job := range reqJSON.Jobs {
-		var jobRun string
-		for strings.TrimSpace(jobRun) != "n" && strings.TrimSpace(jobRun) != "y" {
 
-			// ----display message
-			fmt.Printf("Do you want to start ")
-			msg.Printf(job.Name)
-			msg.Printf("[y/n]:")
+		// ----location
+		execmds := "cd " + job.Loaction
 
-			// ----user input
-			_, err := fmt.Scan(&jobRun)
-			if err != nil {
-				errMsg.Println(err.Error())
+		// ----git pull
+		if job.GitPull {
+			gitCommand := "git pull "
+			if job.DestBranch != "" {
+				gitCommand = gitCommand + " origin " + job.DestBranch
 			}
-
-			// ----user input validation
-			if strings.TrimSpace(jobRun) != "y" && strings.TrimSpace(jobRun) != "n" {
-				errMsg.Println("invalid input! please enter valid input!")
-			}
+			execmds = execmds + " && " + gitCommand
 		}
-		if jobRun == "y" {
-			// ----create execution command
-			cmd := "guake"
-			execmds := "cd " + job.Loaction
 
-			// ----git pull
-			if job.GitPull {
-				gitCommand := "git pull "
-				if job.DestBranch != "" {
-					gitCommand = gitCommand + " origin " + job.DestBranch
-				}
-				execmds = execmds + " && " + gitCommand
-			}
+		// ----other commands
+		for _, execmd := range job.Commands {
+			execmds = execmds + " && " + execmd
+		}
 
-			// ----other commands
-			for _, execmd := range job.Commands {
-				execmds = execmds + " && " + execmd
-			}
+		// ----pass arguments
+		args := []string{"-n", " ", "-r", job.Name, "-e", execmds}
 
-			// ----pass arguments
-			args := []string{"-n", " ", "-r", job.Name, "-e", execmds}
-			if err = exec.Command(cmd, args...).Run(); err != nil {
-				errMsg.Fprintln(os.Stderr, err.Error())
-				os.Exit(1)
-			}
-			fmt.Println("done")
+		// ----execution
+		if err = exec.Command(shell, args...).Run(); err != nil {
+			errMsg.Fprintln(os.Stderr, err.Error())
+			os.Exit(0)
 		}
 	}
 }
